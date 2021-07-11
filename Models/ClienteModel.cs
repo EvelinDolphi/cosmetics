f4fa4f331d6c1cb40e44004dfdb95313e0a7fb9c
Emeris Hernandez <emerisortiz@gmail.com>
2021-07-10 22:30:19 -0500

    Add Cliente Model

---
+++ Models/ClienteModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace cosmetics.Models
+{
+    public class ClienteModel
+    {
+        public int IdCliente { get; set; }
+        public string nombre { get; set; }
+        public string direccion { get; set; }
+    }
+}
