83b4458a3453c24f50ba6d84a745f6ad9cb6a920
Emeris Hernandez <emerisortiz@gmail.com>
2021-07-10 20:27:29 -0500

    First commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace cosmetic_girls.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
