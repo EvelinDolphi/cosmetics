d2ae2bafa02c9756e08760c167f03fe444d3a52d
Evelin Castro <evelincastro021@gmail.com>
2021-07-10 22:39:11 -0500

    Add Category Model

---
+++ Models/CategoriaModel.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace cosmetics.Models
+{
+    public class CategoriaModel
+    {
+        public int IdCategoria { get; set; }
+
+        public string Nombre { get; set; }
+
+    }
+}
