ff21ff5d860cb467ee0a0518019e5b2cb64e9dbd
Evelin Castro <evelincastro021@gmail.com>
2021-07-10 22:42:59 -0500

    Add Sale Model

---
+++ Models/VentasModel.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace cosmetics.Models
+{
+    public class VentasModel
+    {
+        public int IdVentas { get; set; }
+
+        public string FormaPago { get; set; }
+
+        public DateTime Hora { get; set; }
+
+        public string Marca { get; set; }
+
+    }
+}
